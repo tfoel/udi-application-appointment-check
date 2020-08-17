5a320f7c6930d6f9dbabbd39da073482280a4250
Rafael Winterhalter <rafael.wth@gmail.com>
2020-08-17 21:13:09 +0200

    Add better message for failed run.

--- check.sh
+++ check.sh
@@ -7,17 +7,21 @@ ESCAPED_PASSWORD=$(printf '%s\n' "$2" | sed -e 's/[\/&]/\\&/g')
 sed -e "s/%UDI_EMAIL%/$1/" -e "s/%UDI_PASSWORD%/$ESCAPED_PASSWORD/" -e "s/%CHECK_LIMIT%/$3/" check-udi.side > parsed.side
 npx selenium-side-runner -c "browserName=chrome goog:chromeOptions.args=[disable-infobars, headless]" parsed.side > result.txt 2>&1
 rm parsed.side
-if grep -q TimeoutError "result.txt"; then
-  echo "UDI pages did not react within timeout boundaries."
+if grep -q "Assertion failed" "result.txt"; then
+  echo "UDI appointment is available. Quick, before it is taken!"
   rm result.txt
-  exit 0
+  exit 1
 elif grep -q passed "result.txt"; then
   echo "Successfully scanned for appointments but did not find any."
   rm result.txt
   exit 0
+elif grep -q TimeoutError "result.txt"; then
+  echo "UDI pages did not react within timeout boundaries."
+  rm result.txt
+  exit 0
 else
-  echo "Either found available appointments or failed script execution."
+  echo "Script execution failed during execution."
   cat result.txt
   rm result.txt
-  exit 1
+  exit 2
 fi
